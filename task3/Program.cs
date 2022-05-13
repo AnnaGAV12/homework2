25d9051fdc15141bd1f72c8f88934ca69ee534fc
Anna <ds_anna@bk.ru>
2022-05-13 18:09:23 +0300

    По номеру четверти показать диапазоны для возможных координат

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Введите номер четверти ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1) Console.Write($"В {number} четверти диапазон координат X<0 и Y>0");
+if (number == 2) Console.Write($"Во {number} четверти диапазон координат X>0 и Y>0");
+if (number == 3) Console.Write($"В {number} четверти диапазон координат X>0 и Y<0");
+if (number == 4) Console.Write($"В {number} четверти диапазон координат X<0 и Y<0");
